d190cfd6ec9f121af66b2dad144a634ac5261983
Gregor Schneider <gregor.schneider@ionos.com>
2021-11-09 11:24:08 +0100

    added tailwind and craco

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,19 @@
-import React from 'react'
+import React from 'react';
+import {Component} from 'react';
 
-const Header = () => (
-  <header>
-    <nav className="navbar" role="navigation" aria-label="main navigation">
-      <div className="navbar-brand">
-        <a className="navbar-item" href="/">
-          Header Text
-        </a>
-      </div>
-    </nav>
-  </header>
-)
+export default class Header extends Component {
+  render() {
+    return (
+      <header>
+        <nav className="navbar" role="navigation" aria-label="main navigation">
+          <div className="navbar-brand">
+            <a className="navbar-item" href="/">
+              Header Text
+            </a>
+          </div>
+        </nav>
+      </header>
+    );
+  }
+}
 
-export default Header
